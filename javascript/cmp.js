ccaec213a7551d724482f2ef06b7cea2d0f9b781
lucasjdepaola <lucasjdepaola@gmail.com>
2024-04-16 00:00:16 -0400

    added cmp?

---
+++ javascript/cmp.js
@@ -0,0 +1,42 @@
+let cmpIsOn = true; // change during testing
+const getCurrCmp = () => {
+  let start = 0;
+  let end = matrix[coords.row].length - 1;
+  for (let i = coords.col; i < matrix[coords.row].length; i++) {
+    if (lexParseRegex.test(matrix[coords.row][i]) || matrix[coords.row][i] === " ") {
+      end = i;
+      break;
+    }
+  }
+  for (let i = end - 1; i >= 0; i--) {
+    if (lexParseRegex.test(matrix[coords.row][i]) || matrix[coords.row][i] === " ") {
+      start = i + 1;
+      break;
+    }
+  }
+  return matrix[coords.row].slice(start, end).join("");
+}
+
+const fzfArr = (string, arr) => {
+  if (string === "") return arr;
+  string = string.toLowerCase();
+  const a = [];
+  let num = 0;
+  let cache = 0;
+  let flag = false;
+  for (let e of arr) {
+    e = e.toLowerCase();
+    let str = string;
+    for (s of string) {
+      num = e.indexOf(s);
+      if (num === -1 || num < cache) {
+        flag = true;
+      } else {
+        str = str.replace(s, "");
+      }
+    }
+    if (!flag) a.push(e);
+    flag = false;
+  }
+  return a;
+}
