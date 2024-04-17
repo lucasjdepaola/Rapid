76ca9cf1e38ede756fc6bcb3a274eb7789ddff94
lucasjdepaola <lucasjdepaola@gmail.com>
2024-04-17 12:09:31 -0400

    notif displays nicer now

---
+++ javascript/notification.js
@@ -0,0 +1,22 @@
+const REMOVETIME = 5000; // display for 5 seconds
+const notifDiv = document.getElementById("notification");
+const styleMessage = (string, color) => {
+  const node = document.createElement("div");
+  node.style = "white-space:pre-wrap;max-width:200px;overflow-wrap:break-word;margin:3px;z-index:10;border:2px solid " + color + ";border-radius: 5px;padding:5px;background-color:" + currentTheme.background + ";";
+  node.innerText = string;
+  return node;
+}
+
+const notifSuccess = (string) => { sendNotification(string, canvas.success); }
+const notifErr = (string) => { sendNotification(string, canvas.error); }
+
+const sendNotification = (message, color) => {
+  notifDiv.appendChild(styleMessage(message, color));
+  setTimeout(() => { // [1,2,3], 5 seconds pass, [2,3]
+    removeMessage();
+  }, REMOVETIME);
+}
+
+const removeMessage = () => {
+  notifDiv.removeChild(notifDiv.getElementsByTagName("div")[0]); // remove first item?
+}
