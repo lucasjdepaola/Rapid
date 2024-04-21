1ec88918b83e6292508ec61ce5cb331b1e0ecd1e
lucasjdepaola <lucasjdepaola@gmail.com>
2024-04-21 12:11:34 -0400

    added extension example

---
+++ extensions/extensions.js
@@ -0,0 +1,21 @@
+/* load extensions via the web, they don't need to be installed because of how cheap requests are in this case */
+
+const loadExtension = (link) => {
+  const script = document.createElement("script");
+  script.src = link; // XSS has no purpose in this case, there is no backend for a malicious actor to affect
+  document.getElementById("extensions").appendChild(script); // append script to the head
+  script.onload(() => {
+    console.log("script loaded");
+  });
+}
+
+const extensionExample = () => {
+  /* comment all lines out using the param. Example execution: 'commentall //' will comment all with '//' */
+  const commentAll = (commenttype) => {
+    for (const row of matrix) {
+      row.unshift(...commenttype.split("")); // the matrix is a 2d array of chars, so we cannot push pure strings to it
+    }
+  }
+  validCommands["commentall"] = commentAll;
+};
+extensionExample();
--- javascript/main.js
+++ javascript/main.js
@@ -836,118 +836,6 @@ document.addEventListener("touchstart", () => {
   document.getElementById("mobile").focus();
 });
 
-const interpretCommand = (str) => {
-  let cmdstr = commandArr.join("").trim();
-  if (str !== undefined) cmdstr = str;
-  setNormal();
-  commandArr = [];
-  renderCommand(); // so the text goes away
-  // now interpret the command
-  const splitcmd = cmdstr.split(" ");
-  if (splitcmd[0] === "e") {
-    // edit from hashmap here
-    save(currentFilename);
-    if (filemap[splitcmd[1]] === undefined) {
-      filemap[splitcmd[1]] = [[" "]]; // initialize new file
-      if (document.getElementById(splitcmd[1] + "_file") === null) {
-        createFileButton(splitcmd[1]);
-      }
-    }
-    matrix = filemap[splitcmd[1]];
-    unhighlightTab();
-    currentFilename = splitcmd[1];
-    coords.row = 0;
-    coords.col = 0; // can save pos later
-    updateFileName();
-  } else if (splitcmd[0] === "w") {
-    if (splitcmd.length === 2) {
-      currentFilename = splitcmd[1];
-      updateFileName();
-    } else {
-      saveRealFile(currentFilename);
-    }
-  } else if (splitcmd[0] === "background") {
-    if (media[splitcmd[1]] !== undefined) {
-      bg.style.backgroundImage = "url(" + media[splitcmd[1]] + ")";
-      // TODO notification system listing backgrounds, for failed commands, echoing, etc
-    }
-  } else if (cmdstr === "copy") {
-    copyMatrixToOS();
-  } else if (cmdstr === "scope") {
-    toggleScope();
-  }
-  else if (cmdstr === "buffer") {
-    clipTransfer(vimcopybuffer);
-  }
-  else if (splitcmd[0] === "replace") {
-    // replace here
-    replace(splitcmd[1], splitcmd[2]);
-  }
-  else if (cmdstr === "game") {
-    // game here
-    game();
-  }
-  else if (splitcmd[0] === "game") {
-    // easy, medium, hard mode will be implemented
-  }
-  else if (cmdstr === "smooth") {
-    initVide();
-  }
-  else if (cmdstr.toLowerCase() === "explore") {
-    Explore(dirHandle); // browser vim files, could be real files based on the directory, which would be more viable
-  }
-  else if (cmdstr === "source") {
-    sourceConfig();
-  }
-  else if (cmdstr === "file") {
-    pickFiles();
-  }
-  else if (cmdstr === "displaykeys") {
-    keyBufferIsOn = !keyBufferIsOn; // toggle
-  }
-  else if (cmdstr === "smartline") {
-    // smart line function to display tiny numbers per char
-    smartLine = !smartLine;
-  }
-  else if (/^[0-9]+$/.test(cmdstr)) {
-    coords.row = parseInt(cmdstr); // if the string is a number, then go to that line number
-    if (coords.row >= matrix.length) coords.row = matrix.length - 1; // edge case
-  }
-  else if (/[0-9].+:[0-9]+/.test(cmdstr)) { // case of it being line:col
-    const spl = cmdstr.split(":");
-    coords.row = parseInt(spl[0]);
-    coords.col = parseInt(spl[1]) - 1; // do we want it to be zero indexed?
-    if (coords.row >= matrix.length) coords.row = matrix.length - 1;
-    if (coords.col >= matrix[coords.row].length) coords.col = matrix[coords.row].length - 1; // edge cases
-  }
-  else if (cmdstr === "highlight") {
-    isSyntaxHighlighting = !isSyntaxHighlighting; // toggle
-  }
-  else if (splitcmd[0] === "notif") {
-    notif(cmdstr.replace("notif ", ""));
-  }
-  else if (splitcmd[0] === "err") {
-    notifErr(cmdstr.replace("err ", ""));
-  }
-  else if (splitcmd[0] === "warn") {
-    notifWarning(cmdstr.replace("warn ", ""));
-  }
-  else if (splitcmd[0] === "twitch") {
-    injectTMI(splitcmd[1]);
-  }
-  else if (splitcmd[0] === "theme") {
-    if (splitcmd[1] in themeMap) {
-      updateTheme(themeMap[splitcmd[1]]);
-    }
-  }
-  else if (cmdstr === "wpm") {
-    initWPM();
-  }
-  else {
-    sendNotification("Could not find command.", canvas.error)
-  }
-};
-
 const save = (name) => {
   filemap[name] = matrix;
 };
@@ -1170,15 +1058,6 @@ const dispatchCursor = () => {
   let rect = document.getElementById("livecursor").getBoundingClientRect();
   videHandler.move(rect.x, rect.y);
 }
-
-const renderCommand = () => {
-  if (currentState !== states.command) {
-    command.innerHTML = "";
-    return;
-  }
-  command.innerHTML = "$ " + commandArr.join("") +
-    "<span id='livecursor' style='border-left:1px solid white;'> </span>";
-};
 
 const del = (num) => {
   if (matrix[coords.row].length === 1) return; // do not delete the space
